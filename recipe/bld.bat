e0909d9fdcc3f947281d9843e423fd25791e2252
Gregory Lee <grlee77@gmail.com>
2018-11-08 05:14:55 -0500

    add windows build script

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+
+@rem required as fftw3.h installed here
+set C_INCLUDE_PATH="%PREFIX%\include"
+
+@rem tell pyFFTW to search for statically linked libraries
+set STATIC_FFTW_DIR="%LIBRARY_LIB%"
+
+@rem this variable tells pyfftw to search for fftw.lib rather than libfftw.lib
+set PYFFTW_WIN_CONDAFORGE=1
+
+@rem the following commented-out environment variable can be used to specify
+@rem a library search location for dynamic libraries
+@rem set PYFFTW_LIB_DIR="%LIBRARY_LIB%"
+
+%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+if errorlevel 1 exit 1
