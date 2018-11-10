e4dc82d0b44fa47cd49df42a9547fa5879743588
Gregory Lee <grlee77@gmail.com>
2018-11-10 01:50:20 -0500

    try using dynamic FFTW libs instead

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,14 +3,14 @@
 set PYFFTW_INCLUDE=%LIBRARY_INC%
 
 @rem tell pyFFTW to search for statically linked libraries
-set STATIC_FFTW_DIR=%LIBRARY_LIB%
+@rem set STATIC_FFTW_DIR=%LIBRARY_LIB%
 
 @rem this variable tells pyfftw to search for fftw.lib rather than libfftw.lib
 set PYFFTW_WIN_CONDAFORGE=1
 
 @rem the following commented-out environment variable can be used to specify
 @rem a library search location for dynamic libraries
-@rem set PYFFTW_LIB_DIR="%LIBRARY_BIN%"
+set PYFFTW_LIB_DIR=%LIBRARY_BIN%
 
 @rem python setup.py build_ext -n -IF:\Miniconda3\conda-bld\pyfftw_1541828673207\_h_env\Library\include
 %PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
