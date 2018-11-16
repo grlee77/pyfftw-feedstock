a7bd9e0a6540a1aae79c246a202873d77d2e8373
Gregory R. Lee <grlee77@gmail.com>
2018-11-16 17:05:48 -0500

    remove extraneous comments

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,9 +2,6 @@
 @rem required as fftw3.h installed here
 set PYFFTW_INCLUDE=%LIBRARY_INC%
 
-@rem tell pyFFTW to search for statically linked libraries
-@rem set STATIC_FFTW_DIR=%LIBRARY_LIB%
-
 @rem this variable tells pyfftw to search for fftw.lib rather than libfftw.lib
 set PYFFTW_WIN_CONDAFORGE=1
 
@@ -12,6 +9,5 @@ set PYFFTW_WIN_CONDAFORGE=1
 @rem a library search location for dynamic libraries
 set PYFFTW_LIB_DIR=%LIBRARY_BIN%
 
-@rem python setup.py build_ext -n -IF:\Miniconda3\conda-bld\pyfftw_1541828673207\_h_env\Library\include
 %PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 if errorlevel 1 exit 1
