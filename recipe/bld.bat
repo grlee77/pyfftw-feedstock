2eac3018b2957fcfda2c0e7cdfea837d2304ce5e
Gregory R. Lee <grlee77@gmail.com>
2018-11-16 17:08:37 -0500

    remove additional outdated comment

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,6 @@ set PYFFTW_INCLUDE=%LIBRARY_INC%
 @rem this variable tells pyfftw to search for fftw.lib rather than libfftw.lib
 set PYFFTW_WIN_CONDAFORGE=1
 
-@rem the following commented-out environment variable can be used to specify
 @rem a library search location for dynamic libraries
 set PYFFTW_LIB_DIR=%LIBRARY_BIN%
 
